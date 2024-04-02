ad7dc7a431f06f0046fb1b428248fb134263dc51
GregBiglione <gregory.biglione81@gmail.com>
2024-04-03 01:11:18 +0200

    Added RouteManager

---
+++ lib/presentation/resource/route_manager.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_auth_app/presentation/resource/color_manager.dart';
+import 'package:google_auth_app/presentation/resource/string_manager.dart';
+import 'package:google_auth_app/presentation/screen/signin/signin_screen.dart';
+import 'package:google_auth_app/presentation/screen/splash/splash_screen.dart';
+
+class Routes {
+  static const splashRoute = "/";
+  static const signInRoute = "/signIn";
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings routeSettings) {
+    switch(routeSettings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+      case Routes.signInRoute:
+        return MaterialPageRoute(builder: (_) => const SignInScreen());
+      default:
+        return undefinedRoute();
+    }
+  }
+
+  static Route<dynamic> undefinedRoute() {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(
+          title: Text(
+            StringManager.noRouteFound,
+            style: Theme.of(_).textTheme.displaySmall,
+          ),
+        ),
+        backgroundColor: ColorManager.black,
+        body: Center(
+          child: Text(
+            StringManager.noRouteFound,
+            style: Theme.of(_).textTheme.displaySmall,
+          ),
+        ),
+      ),
+    );
+  }
+}

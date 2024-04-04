1a4c9d66c40ff6394ec0320c8d703fb587a7ea22
GregBiglione <gregory.biglione81@gmail.com>
2024-04-04 11:38:49 +0200

    Added _googleButtonWidget() in SignInContent

--- lib/app/constant.dart
+++ lib/app/constant.dart
@@ -1,1 +1,4 @@
+import 'package:logger/logger.dart';
+
+var logger = Logger();
 const String IMAGE_PATH = "assets/images";
--- lib/presentation/screen/signin/widget/signin_content.dart
+++ lib/presentation/screen/signin/widget/signin_content.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_auth_app/app/constant.dart';
 import 'package:google_auth_app/presentation/resource/string_manager.dart';
 import 'package:google_auth_app/presentation/resource/value_manager.dart';
+import '../../../resource/color_manager.dart';
+import '../../../resource/font_manager.dart';
+import '../../../resource/image_manager.dart';
+import '../../../resource/style_manager.dart';
 
 class SignInContent extends StatefulWidget {
   const SignInContent({super.key});
@@ -31,7 +36,8 @@ class _SignInContentState extends State<SignInContent> {
         _signInTitleWidget(),
         const SizedBox(
           height: ValueManager.v30,
-        )
+        ),
+        _googleButtonWidget(),
       ],
     ),
   );
@@ -51,4 +57,74 @@ class _SignInContentState extends State<SignInContent> {
     ),
   );
 
+  //----------------------------------------------------------------------------
+  // Google button
+  //----------------------------------------------------------------------------
+
+  Widget _googleButtonWidget() => GestureDetector(
+    child: Padding(
+      padding: const EdgeInsets.only(
+        left: ValueManager.v16,
+        right: ValueManager.v16,
+      ),
+      child: Container(
+        width: ValueManager.infinity,
+        height: ValueManager.v50,
+        decoration: BoxDecoration(
+          color: ColorManager.primary,
+          border: Border.all(
+            color: ColorManager.primary,
+            width: ValueManager.v2,
+          ),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(
+              ValueManager.v30,
+            ),
+          ),
+        ),
+        child: Row(
+          children: [
+            // Logo --------------------------------------------------------------
+            Container(
+              decoration: BoxDecoration(
+                color: ColorManager.white,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(
+                    ValueManager.v30,
+                  ),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(
+                  ValueManager.v8,
+                ),
+                child: Image.asset(
+                  ImageManager.splashLogo,
+                ),
+              ),
+            ),
+            // Title -------------------------------------------------------------
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  right: ValueManager.v46,
+                ),
+                child: Text(
+                  StringManager.googleButtonTitle,
+                  style: getRegularStyle(
+                    fontSize: FontSize.s18,
+                    color: ColorManager.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+    onTap: () {
+      logger.i("Google button clicked");
+    },
+  );
 }

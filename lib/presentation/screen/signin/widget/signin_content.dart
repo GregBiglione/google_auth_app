c5452af73c3446c3fcd13345e04799728f413a02
GregBiglione <gregory.biglione81@gmail.com>
2024-04-04 06:00:16 +0200

    Added _getContentWidget() & _signInTitleWidget() in SignInContent

--- lib/presentation/screen/signin/widget/signin_content.dart
+++ lib/presentation/screen/signin/widget/signin_content.dart
@@ -1,18 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:google_auth_app/presentation/resource/string_manager.dart';
+import 'package:google_auth_app/presentation/resource/value_manager.dart';
 
-class SignInContent extends StatelessWidget {
+class SignInContent extends StatefulWidget {
   const SignInContent({super.key});
 
   @override
+  State<SignInContent> createState() => _SignInContentState();
+}
+
+class _SignInContentState extends State<SignInContent> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Sign in screen",
-        style: TextStyle(
-          fontSize: 20,
-          color: Colors.white,
-        ),
-      ),
-    );
+    return _getContentWidget();
   }
+
+  //----------------------------------------------------------------------------
+  // All content
+  //----------------------------------------------------------------------------
+
+  Widget _getContentWidget() => Padding(
+    padding: const EdgeInsets.only(
+      left: ValueManager.v16,
+      right: ValueManager.v16,
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _signInTitleWidget(),
+        const SizedBox(
+          height: ValueManager.v30,
+        )
+      ],
+    ),
+  );
+
+  //----------------------------------------------------------------------------
+  // Title
+  //----------------------------------------------------------------------------
+
+  Widget _signInTitleWidget() => Padding(
+    padding: const EdgeInsets.only(
+      left: ValueManager.v16,
+      right: ValueManager.v16,
+    ),
+    child: Text(
+      StringManager.signInTitle,
+      style: Theme.of(context).textTheme.displayMedium,
+    ),
+  );
+
 }

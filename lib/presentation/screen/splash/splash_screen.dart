ceb2bad006a240eb9cb56c9a3ced7485764868f5
GregBiglione <gregory.biglione81@gmail.com>
2024-04-04 03:17:20 +0200

    Added timer in SplashScreen & hide status bar

--- lib/presentation/screen/splash/splash_screen.dart
+++ lib/presentation/screen/splash/splash_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_auth_app/presentation/resource/image_manager.dart';
+import 'package:google_auth_app/presentation/resource/route_manager.dart';
 import 'package:google_auth_app/presentation/resource/string_manager.dart';
 import 'package:google_auth_app/presentation/resource/style_manager.dart';
 import 'package:google_auth_app/presentation/resource/value_manager.dart';
@@ -15,8 +19,46 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  Timer? _timer;
+
+  //----------------------------------------------------------------------------
+  // Go to Sign In Screen
+  //----------------------------------------------------------------------------
+
+  _goToSignInScreen() {
+    Navigator.pushReplacementNamed(
+      context,
+      Routes.signInRoute,
+    );
+  }
+
+  //----------------------------------------------------------------------------
+  // Start timer
+  //----------------------------------------------------------------------------
+
+  _startTimer() {
+    _timer = Timer(
+      const Duration(seconds: 4),
+      _goToSignInScreen,
+    );
+  }
+
+  @override
+  void initState() {
+    _startTimer();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
     return Scaffold(
       backgroundColor: ColorManager.white,
       body: Center(

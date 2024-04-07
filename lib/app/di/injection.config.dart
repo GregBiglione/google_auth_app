7a681ea4ea71b773daea71cddd4f7c9eeae5cc72
GregBiglione <gregory.biglione81@gmail.com>
2024-04-07 16:24:47 +0200

    Added GoogleSignInUseCase & AuthUseCase

--- lib/app/di/injection.config.dart
+++ lib/app/di/injection.config.dart
@@ -8,10 +8,13 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:firebase_auth/firebase_auth.dart' as _i3;
+import 'package:firebase_auth/firebase_auth.dart' as _i5;
 import 'package:get_it/get_it.dart' as _i1;
-import 'package:google_auth_app/app/di/app_module.dart' as _i5;
-import 'package:google_auth_app/app/di/firebase_service.dart' as _i4;
+import 'package:google_auth_app/app/di/app_module.dart' as _i7;
+import 'package:google_auth_app/app/di/firebase_service.dart' as _i6;
+import 'package:google_auth_app/domain/repository/auth/auth_repository.dart'
+    as _i3;
+import 'package:google_auth_app/domain/usecase/auth/auth_usecase.dart' as _i4;
 import 'package:injectable/injectable.dart' as _i2;
 
 extension GetItInjectableX on _i1.GetIt {
@@ -26,8 +29,10 @@ extension GetItInjectableX on _i1.GetIt {
       environmentFilter,
     );
     final appModule = _$AppModule();
-    gh.factory<_i3.FirebaseAuth>(() => appModule.firebaseAuth);
-    await gh.factoryAsync<_i4.FirebaseService>(
+    gh.factory<_i3.AuthRepository>(() => appModule.authRepository);
+    gh.factory<_i4.AuthUseCase>(() => appModule.authUseCase);
+    gh.factory<_i5.FirebaseAuth>(() => appModule.firebaseAuth);
+    await gh.factoryAsync<_i6.FirebaseService>(
       () => appModule.firebaseService,
       preResolve: true,
     );
@@ -35,4 +40,4 @@ extension GetItInjectableX on _i1.GetIt {
   }
 }
 
-class _$AppModule extends _i5.AppModule {}
+class _$AppModule extends _i7.AppModule {}
---
+++ lib/domain/usecase/auth/auth_usecase.dart
@@ -0,0 +1,7 @@
+import 'google_signin_usecase.dart';
+
+class AuthUseCase {
+  final GoogleSignInUseCase googleSignInUseCase;
+
+  AuthUseCase({required this.googleSignInUseCase});
+}

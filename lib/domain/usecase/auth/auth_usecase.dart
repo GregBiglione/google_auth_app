626ad0a2b23de19ee8e31b2b15ea1107aefe89c2
GregBiglione <gregory.biglione81@gmail.com>
2024-04-09 07:01:38 +0200

    Added User in AuthRepository & AuthRepositoryImplementer & added UserSessionUseCase

--- lib/domain/usecase/auth/auth_usecase.dart
+++ lib/domain/usecase/auth/auth_usecase.dart
@@ -1,7 +1,11 @@
+import 'package:google_auth_app/domain/usecase/auth/user_session_usecase.dart';
+
 import 'google_signin_usecase.dart';
 
 class AuthUseCase {
+  final UserSessionUseCase userSessionUseCase;
   final GoogleSignInUseCase googleSignInUseCase;
 
-  AuthUseCase({required this.googleSignInUseCase});
+  AuthUseCase({required this.userSessionUseCase,
+    required this.googleSignInUseCase});
 }

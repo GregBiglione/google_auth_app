9864b89b1f389fe304a111943f172fc488f1ab70
GregBiglione <gregory.biglione81@gmail.com>
2024-04-09 16:04:55 +0200

    Added logoutNavigation() in Function

--- lib/app/function.dart
+++ lib/app/function.dart
@@ -1,6 +1,9 @@
 // Error toast -----------------------------------------------------------------
 
 import 'package:flutter/material.dart';
+import 'package:google_auth_app/app/di/injection.dart';
+import 'package:google_auth_app/domain/usecase/auth/auth_usecase.dart';
+import 'package:google_auth_app/main.dart';
 import 'package:google_auth_app/presentation/resource/color_manager.dart';
 import 'package:google_auth_app/presentation/resource/route_manager.dart';
 import 'package:toastification/toastification.dart';
@@ -25,3 +28,14 @@ void goToHomeScreen(BuildContext context) => WidgetsBinding.instance
         Routes.homeRoute,
       );
 });
+
+// Go to main after logout -----------------------------------------------------
+
+void logoutNavigation(BuildContext context) =>
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MyApp(getIt<AuthUseCase>()),
+      ),
+      (route) => false,
+);
